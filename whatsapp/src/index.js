77f17796ef044dac15f4e6b9ddd0d462f64f06c5
sercan masar <smasar@outlook.com>
2022-03-22 14:29:42 +0300

    adding provider context api

--- whatsapp/src/index.js
+++ whatsapp/src/index.js
@@ -7,7 +7,9 @@ import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
